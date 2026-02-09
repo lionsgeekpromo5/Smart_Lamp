bb8d4b512fd2087e680e5cd23e5ec47faa3ea2cc
lionsgeekpromo5 <sellingjewelery62@gmail.com>
2026-02-09 14:46:05 +0100

    add scale animation to image

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from "react";
+import { motion } from "motion/react";
 
-function Card({src, title, price}) {
+function Card({ src, title, price }) {
   return (
-    <div className='w-[20vw] h-[60vh]'>
-        <img src={src} alt="" className='w-full h-[80%]' />
-        <h4>{title}</h4>
-        <p>From ${price} </p>
+    <div className="w-[20vw] h-[60vh] bg-green-60">
+      <div className="h-[80%] overflow-hidden">
+        <motion.img
+          whileHover={{ scale: 1.1, transition: {duration: 0.6} }}
+          src={src}
+          alt=""
+          className="w-full  h-full "
+        />
+      </div>
+      <h4>{title}</h4>
+      <p>From ${price} </p>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
